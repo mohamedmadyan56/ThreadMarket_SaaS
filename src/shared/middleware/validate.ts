4cb88e5bba7867fac22862fc114c272f73a43e59
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-25 18:11:42 +0300

    Refactor:this is Zod Validation

---
+++ src/shared/middleware/validate.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+import AppError from "../errors/AppError";
+import { type Request, type Response, type NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const validate = (schema: z.ZodSchema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = { ...req.body, ...req.query, ...req.params };
+      const parsedData = schema.parse(data);
+      if (parsedData) next();
+    } catch (error) {
+      if (error && error instanceof z.ZodError) {
+        return next(
+          new AppError(error.issues[0]!.message, StatusCodes.BAD_REQUEST),
+        );
+      }
+    }
+  };
+};
