3b52b1fbc13532ab5a00b27b99e45c223706e9c4
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-25 04:35:00 +0300

    feat:Refactor middlewares/isAuthorized.ts

---
+++ src/shared/middleware/authorize.ts
@@ -0,0 +1,17 @@
+import { type Response, type NextFunction } from "express";
+import AppError from "../errors/AppError";
+import { StatusCodes } from "http-status-codes";
+
+export const authorize = (...roles: string[]) => {
+  return async (req: any, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(
+          "You are not Authorized to Access This Route",
+          StatusCodes.FORBIDDEN,
+        ),
+      );
+    }
+    return next();
+  };
+};
