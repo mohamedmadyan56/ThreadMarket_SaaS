42fff4427157b2096b85d9a860a90d8b860e84b2
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-27 05:08:06 +0300

    last update for multer

--- src/shared/utils/upload.ts
+++ src/shared/utils/upload.ts
@@ -1,6 +1,6 @@
 import multer from "multer";
 import fs from "fs";
-
+import path from "path";
 const uploadDir = "./src/uploads";
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
