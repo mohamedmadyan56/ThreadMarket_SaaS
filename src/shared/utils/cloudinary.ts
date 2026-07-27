8ca2881a519cc3d643a77c13fff8147816ebb1ca
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-27 04:13:00 +0300

    feat:Added UploadBufferToCloudinary

--- src/shared/utils/cloudinary.ts
+++ src/shared/utils/cloudinary.ts
@@ -1,6 +1,8 @@
 import { v2 as cloudinary,UploadApiResponse } from "cloudinary";
 import { ENV } from "../../config/env";
 import path from "path";
+import { resolve } from "dns";
+import { rejects } from "assert";
 
 cloudinary.config({
   cloud_name: ENV.CLOUDINARY_NAME,
@@ -22,6 +24,30 @@ export const uploadToCloudinary = async (filePath: string, folder: string):Promi
         throw new AppError(      error?.message || "Cloudinary upload failed",500)
     }
 };
+
+export const uploadBufferToCloudinary = async (
+  buffer: Buffer,
+  folder: string
+): Promise<UploadApiResponse> => {
+  return new Promise<UploadApiResponse>((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { folder },
+      (error, result) => {
+        if (error || !result) {
+          reject(new AppError(error?.message || "Buffer upload failed", 500));
+        } else {
+          resolve(result);
+        }
+      }
+    );
+    stream.end(buffer);
+  });
+};
+
+
+
+
+
 
 export const deleteFromCloudinary = async (
   public_Id: string,
