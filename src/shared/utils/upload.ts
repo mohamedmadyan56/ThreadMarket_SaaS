551cc659fe6e6d4f9c99654db0dccb747077339e
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-27 04:37:11 +0300

    Feat:This is RemoveFromDiskStroage

--- src/shared/utils/upload.ts
+++ src/shared/utils/upload.ts
@@ -12,3 +12,26 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadDir),
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 });
+
+
+const fileFilter = (req: any, file: any, cb: any) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+export const upload = multer({ storage, fileFilter });
+
+
+
+
+export const removeFileFromDisk = (filePath: string) => {
+  try {
+    const resolvedPath = path.resolve(filePath);
+    if (fs.existsSync(resolvedPath)) {
+      fs.unlinkSync(resolvedPath);
+    }
+  } catch (error) { console.log(error); }
+};
