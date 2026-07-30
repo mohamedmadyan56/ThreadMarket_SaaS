56d9170af68f441430c741cd5808d971c697bfc3
mohamed madyan <mohamedmadyan31@gmail.com>
2026-07-30 16:58:10 +0300

    implement brand controller

---
+++ src/modules/brand/brand.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { brandService } from "./brand.service";
+import { StatusCodes } from "http-status-codes";
+import { asyncHandler } from "../../shared/utils/asyncHandler";
+import AppError from "../../shared/errors/AppError";
+
+export const brandVerificationStatus = asyncHandler(async (req, res) => {
+    const { brandId } = req.params as { brandId: string };
+    const user = (req as any).user;
+
+    if (!user?.id) {
+        throw new AppError("Not authenticated", 401);
+    }
+
+    const result = await brandService.getBrandVerificationStatus(brandId, user.id, user.role);
+
+    return res.status(200).json({ success: true, message: "OK", data: result });
+});
