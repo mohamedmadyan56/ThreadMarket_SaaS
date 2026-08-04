3b2d7f1d538d0af98e0787a3603a03792cf10930
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-04 16:53:42 +0300

    feat(category): add search by name or description

--- src/modules/category/category.model.ts
+++ src/modules/category/category.model.ts
@@ -1,24 +1,19 @@
 import prisma from "../../config/database";
 
+
 class categoryModel {
-  async getCategories(filter: {
-    search: string;
-    skip?: number;
-    take?: number;
-  }) {
+  async getCategories(filter: { search: string; skip?: number; take?: number; }) {
     return await prisma.category.findMany({
       where: {
-        name: {
-          contains: filter.search,
-        },
-        description: {
-          contains: filter.search,
-        },
+        OR: [
+          { name: { contains: filter.search } },
+          { description: { contains: filter.search } },
+        ]
+
       },
       skip: filter.skip,
       take: filter.take,
-    });
+    })
   }
-}
 
-export const category = new categoryModel();
+}
