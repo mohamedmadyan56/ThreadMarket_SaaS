c40d124f13ce91114f84edf6cd31e5877877b9e8
mohamed madyan <mohamedmadyan31@gmail.com>
2026-08-04 21:51:48 +0300

    refactor(category): organize category service methods

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,11 +1,46 @@
-import { category } from "./category.model";
+import {
+  categoryModel,
+  type CategoryFilter,
+  type CategoryResult,
+  type CreateCategoryInput,
+  type UpdateCategoryInput,
+} from "./category.model";
+
 class CategoryService {
-  async getCategories(filter: {
-    search: string;
-    skip?: number;
-    take?: number;
-  }) {
-    return await category.getCategories(filter);
+
+  async getCategories(filter: CategoryFilter): Promise<CategoryResult<any>> {
+    return await categoryModel.getCategories(filter);
+  }
+
+  async getCategoryById(id: string) {
+    return await categoryModel.getCategoryById(id);
+  }
+
+  async createCategory(data: CreateCategoryInput) {
+    const createData = {
+      name: data.name,
+      description: data.description,
+      media: data.media ?? {},
+    };
+    return await categoryModel.createCategory(createData);
+  }
+  async updateCategory(id: string, data: UpdateCategoryInput) {
+    const updateData: Record<string, any> = {};
+    if (data.name !== undefined) updateData.name = data.name;
+    if (data.description !== undefined) updateData.description = data.description;
+    if (data.media !== undefined) updateData.media = data.media;
+    if (data.isHidden !== undefined) updateData.ishidden = data.isHidden;
+
+    return await categoryModel.updateCategory(id, updateData);
+  }
+
+
+  async deleteCategory(id: string, soft = true) {
+    return await categoryModel.deleteCategory(id, soft);
+  }
+
+  async toggleVisibility(id: string) {
+    return await categoryModel.toggleVisibility(id);
   }
 }
 
